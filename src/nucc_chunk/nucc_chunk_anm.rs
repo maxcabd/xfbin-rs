c80b07009c73a9d1548b1fddd37b57c7b4a7ccf2
maxcabd <84046498+maxcabd@users.noreply.github.com>
2024-06-06 20:14:43 -0400

    Rewrite reading/writing for nuccChunkAnm

--- src/nucc_chunk/nucc_chunk_anm.rs
+++ src/nucc_chunk/nucc_chunk_anm.rs
@@ -2,17 +2,15 @@
 //! nuccAnm is a chunk that contains animation data.
 //! The extension ".anm" stands for "Animation".
 //!
-use binrw::{binrw, BinRead, BinResult, ReadOptions};
-use hashbrown::hash_map::Values;
-use std::io::{Read, Seek};
+use std::{default, io::{Read, Seek, Write}};
+use binrw::{binrw, BinRead, BinResult, BinWrite, ReadOptions, WriteOptions};
 
-use super::nucc_helper::*;
 use super::{NuccChunk, NuccChunkType};
 
 #[binrw]
 #[brw(big)]
 #[br(import_raw(version: u16))]
-#[derive(Debug, Clone, PartialEq, Default)]
+#[derive(Debug, Clone, Default)]
 pub struct NuccChunkAnm {
     #[brw(ignore)]
     pub version: u16,
@@ -73,6 +71,7 @@ pub struct AnmClump {
     pub model_indices: Vec<u32>,
 }
 
+
 #[binrw]
 #[derive(Debug, Clone, PartialEq)]
 pub struct CoordParent {
@@ -80,277 +79,165 @@ pub struct CoordParent {
     pub child: AnmCoord,
 }
 
+
 #[binrw]
-#[derive(Debug, Clone, PartialEq)]
+#[derive(Debug, Clone, PartialEq, Default)]
 pub struct AnmCoord {
     pub clump_index: i16,
     pub coord_index: u16,
 }
 
-#[binrw]
-#[brw(repr(u16))]
-#[derive(Debug, Clone)]
-pub enum AnmEntryFormat {
-    BONE = 1,
-    CAMERA = 2,
-    MATERIAL = 4,
-    LIGHTDIRC = 5,
-    LIGHTPOINT = 6,
-    AMBIENT = 8,
-    MORPHMODEL = 9,
-}
+
+
 
 #[binrw]
-#[derive(Debug, Clone, PartialEq)]
+#[brw(repr(u16))]
+#[derive(Debug, Clone, Default)]
+pub enum EntryFormat {
+    Coord = 1,
+    Camera = 2,
+    Material = 4,
+    LightDirc = 5,
+    LightPoint = 6,
+    Ambient = 8,
+    MorphModel = 9,
+
+    #[default]
+    Unknown,
+}
+
+
+#[binrw]
+#[derive(Debug, Clone)]
 pub struct AnmEntry {
     pub coord: AnmCoord,
-    pub entry_format: u16,
+    pub entry_format: EntryFormat,
+
+    #[bw(calc = curve_headers.len() as u16)]
     pub curve_count: u16,
 
     #[br(count = curve_count)]
     pub curve_headers: Vec<CurveHeader>,
 
-    #[br(parse_with = from_iterator_args(curve_headers.iter()))]
+    #[br(parse_with = read_curve(curve_headers.iter()))]
     #[br(align_after = 4)]
     pub curves: Vec<Curve>,
 }
+
 
 #[binrw]
 #[derive(Debug, Clone, PartialEq)]
 pub struct CurveHeader {
     pub curve_index: u16,
-    pub curve_format: u16,
+    pub curve_format: CurveFormat,
     pub frame_count: u16,
     pub curve_size: u16,
 }
 
+
 #[binrw]
 #[brw(repr(u16))]
-#[derive(Debug)]
-#[allow(non_camel_case_types)]
-pub enum AnmCurveFormat {
-    FLOAT3 = 0x05,      // location/scale
-    INT1_FLOAT3 = 0x06, // location/scale (with keyframe)
-    FLOAT3ALT = 0x08,   // rotation
-    INT1_FLOAT4 = 0x0A, // rotation quaternions (with keyframe)
-    FLOAT1 = 0x0B,      // "toggled"
-    INT1_FLOAT1 = 0x0C, // camera
-    SHORT1 = 0x0F,      // "toggled"
-    SHORT3 = 0x10,      // scale
-    SHORT4 = 0x11,      // rotation quaternions
-    SHORT1_ALT = 0x1D,  // 
-    BYTE3 = 0x14,       // lightdirc
-    FLOAT3ALT2 = 0x15,  // scale
-    FLOAT1ALT = 0x16,   // lightdirc
-    FLOAT1ALT2 = 0x18,  // material
-   
+#[derive(Debug, Clone, PartialEq, Default)]
+pub enum CurveFormat {
+    Vector3Fixed = 0x5,
+    Vector3Linear = 0x6,
+    Vector3Bezier = 0x7,
+    EulerXYZFixed  = 0x8,
+    EulerInterpolated = 0x9,
+    QuaternionLinear = 0xA,
+    FloatFixed = 0xB,
+    FloatLinear = 0xC,
+    Vector2Fixed = 0xD,
+    Vector2Linear = 0xE,
+    OpacityShortTable = 0xF,
+    ScaleShortTable = 0x10,
+    QuaternionShortTable = 0x11,
+    ColorRGBTable = 0x14,
+    Vector3Table = 0x15,
+    FloatTable = 0x16,
+    QuaternionTable = 0x17,
+    FloatTableNoInterp = 0x18,
+    Vector3ShortLinear = 0x19,
+    Vector3TableNoInterp = 0x1A,
+    QuaternionShortTableNoInterp = 0x1B,
+    OpacityShortTableNoInterp = 0x1D,
+
+    #[default]
+    Unknown,
+}
+
+impl CurveFormat {
+    pub fn size_per_frame(&self) -> usize {
+        match self {
+            CurveFormat::OpacityShortTable | CurveFormat::OpacityShortTableNoInterp => 0x02,
+            CurveFormat::ColorRGBTable => 0x03,
+            CurveFormat::FloatFixed | CurveFormat::FloatTable | CurveFormat::FloatTableNoInterp => {
+                0x04
+            }
+            CurveFormat::ScaleShortTable => 0x06,
+            CurveFormat::FloatLinear
+            | CurveFormat::Vector2Fixed
+            | CurveFormat::QuaternionShortTable
+            | CurveFormat::QuaternionShortTableNoInterp => 0x08,
+            CurveFormat::Vector3Fixed
+            | CurveFormat::EulerXYZFixed
+            | CurveFormat::Vector2Linear
+            | CurveFormat::Vector3Table
+            | CurveFormat::Vector3TableNoInterp => 0x0C,
+            CurveFormat::Vector3Linear | CurveFormat::QuaternionTable => 0x10,
+            CurveFormat::QuaternionLinear => 0x14,
+            CurveFormat::Vector3Bezier => todo!(),
+            CurveFormat::EulerInterpolated => todo!(),
+            CurveFormat::Vector3ShortLinear => todo!(),
+            CurveFormat::Unknown => todo!(),
+        }
+    }
 
 }
 
 #[binrw]
 #[derive(Debug, Clone, PartialEq)]
+pub enum Math {
+    Vec3 { channels: (f32, f32, f32) },
+    Vec3Linear { frame: i32, channels: (f32, f32, f32) },
+    Vec4Linear { frame: i32, channels: (f32, f32, f32, f32) },
+    Float { channels: f32 },
+    FloatLinear { frame: i32, channels: f32 },
+    I16Vec { channels: i16 },
+    I16Vec3 { channels: (i16, i16, i16) },
+    I16Vec4 { channels: (i16, i16, i16, i16) },
+    Color { channels: (u8, u8, u8) },
+
+    // Add more variants as needed...
+    Unknown {}
+    
+}
+
+impl default::Default for Math {
+    fn default() -> Self {
+        Math::Unknown {}
+    }
+}
+
+
+#[binrw]
 #[br(import_raw(header: CurveHeader))]
-pub enum Curve {
-    #[br(pre_assert(matches!(header.curve_format, 0x05 | 0x08 | 0x15 | 0x1A)))]
-    Vector3(#[br(count = header.frame_count)] Vec<Vector3>),
+#[derive(Debug, Clone, PartialEq)]
+pub struct Curve {
+    #[brw(ignore)]
+    pub curve_format: CurveFormat, // Used for writing the data
 
-    #[br(pre_assert(matches!(header.curve_format, 0x06)))]
-    KeyframeVector3(#[br(count = header.frame_count)] Vec<KeyframeVector3>),
+    #[brw(ignore)]
+    pub keyframe: Math,
 
-    #[br(pre_assert(matches!(header.curve_format, 0x0A)))]
-    KeyframeVector4(#[br(count = header.frame_count)] Vec<KeyframeVector4>),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x0B | 0x16 | 0x18)))]
-    Float(#[br(count = header.frame_count)] Vec<f32>),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x0C)))]
-    KeyframeFloat(#[br(count = header.frame_count)] Vec<KeyframeFloat>),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x0F)))]
-    Short(
-        #[br(count = header.frame_count)] Vec<i16>,
-    ),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x10)))]
-    Vector3Short(
-        #[br(count = header.frame_count)]
-        Vec<Vector3Short>,
-    ),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x1D)))]
-    VectorShort(
-        #[br(count = header.frame_count)]
-        Vec<VectorShort>,
-    ),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x11 | 0x1B)))]
-    QuaternionShort(#[br(count = header.frame_count)] Vec<QuaternionShort>),
-
-    #[br(pre_assert(matches!(header.curve_format, 0x14)))]
-    RGB(#[br(count = header.frame_count)] Vec<RGB>),
-
-    // Handle unknown curve formats
-    #[br(pre_assert(false))]
-    Unknown(#[br(count = header.curve_size)] Vec<u8>),
+    #[br(count = header.curve_format.size_per_frame() * header.frame_count as usize)]
+    #[bw(write_with = |_data, writer, wo, () | write_curve(curve_format.clone(), keyframe.clone(), writer, wo, ()))]
+    pub data: Vec<u8>,
 }
 
-impl Curve {
-    pub fn get_frame_count(&self) -> u16 {
-        match self {
-            Curve::Vector3(curve) => curve.len() as u16,
-            Curve::KeyframeVector3(curve) => curve.len() as u16,
-            Curve::KeyframeVector4(curve) => curve.len() as u16,
-            Curve::Float(curve) => curve.len() as u16,
-            Curve::KeyframeFloat(curve) => curve.len() as u16,
-            Curve::Short(curve) => curve.len() as u16,
-            Curve::Vector3Short(curve) => curve.len() as u16,
-            Curve::VectorShort(curve) => curve.len() as u16,
-            Curve::QuaternionShort(curve) => curve.len() as u16,
-            Curve::RGB(curve) => curve.len() as u16,
-            Curve::Unknown(curve) => curve.len() as u16,
-        }
-    }
 
-    pub fn has_keyframes(&self) -> bool {
-        if &self.get_frame_count() > &1 {
-            return true;
-        }
-        return false;
-    }
 
-    pub fn get_curve_format(&self) -> u16 {
-        match self {
-            Curve::Vector3(_) => AnmCurveFormat::FLOAT3 as u16,
-            Curve::KeyframeVector3(_) => AnmCurveFormat::INT1_FLOAT3 as u16,
-            Curve::KeyframeVector4(_) => AnmCurveFormat::INT1_FLOAT4 as u16,
-            Curve::Float(_) => AnmCurveFormat::FLOAT1 as u16,
-            Curve::KeyframeFloat(_) => AnmCurveFormat::INT1_FLOAT1 as u16,
-            Curve::Short(_) => AnmCurveFormat::SHORT1 as u16,
-            Curve::Vector3Short(_) => AnmCurveFormat::SHORT3 as u16,
-            Curve::VectorShort(_) => AnmCurveFormat::SHORT1_ALT as u16,
-            Curve::QuaternionShort(_) => AnmCurveFormat::SHORT4 as u16,
-            Curve::RGB(_) => AnmCurveFormat::BYTE3 as u16,
-            Curve::Unknown(_) => AnmCurveFormat::FLOAT1ALT2 as u16,
-        }
-    }
-
-    pub fn pad_values(&mut self) {
-        match self {
-
-            Curve::Short(values) => {
-                let len = values.len();
-
-                if len % 2 != 0 {
-                    let last_value = match values.last() {
-                        Some(value) => value.clone(),
-                        None => 0, // Provide a default value if the curve is empty
-                    };
-
-                    for _ in len % 2..2 {
-                        values.push(last_value.clone());
-                    }
-                }
-            }
-            
-
-            Curve::Vector3Short(values) => {
-                let len = values.len();
-
-                if len % 2 != 0 {
-                    let last_vector = match values.last() {
-                        Some(vector) => vector.clone(),
-                        None => Vector3Short {
-                            x: 4960,
-                            y: 4960,
-                            z: 4960,
-                        }, // Provide a default vector if the curve is empty
-                    };
-
-                    for _ in len % 2..2 {
-                        values.push(last_vector.clone());
-                    }
-                }
-            }
-
-            Curve::VectorShort(values) => {
-                let len = values.len();
-
-                if len % 4 != 0 {
-                    let last_vector = match values.last() {
-                        Some(vector) => vector.clone(),
-                        None => VectorShort { x: 4960 }, // Provide a default vector if the curve is empty
-                    };
-
-                    for _ in len % 4..4 {
-                        values.push(last_vector.clone());
-                    }
-                }
-            }
-
-            Curve::RGB(values) => {
-                let len = values.len();
-
-                if len % 4 != 0 {
-                    let last_color = match values.last() {
-                        Some(color) => color.clone(),
-                        None => RGB {
-                            r: 255,
-                            g: 255,
-                            b: 255,
-                        }, // Provide a default color if the curve is empty
-                    };
-
-                    for _ in len % 4..4 {
-                        values.push(last_color.clone());
-                    }
-                }
-            }
-            _ => {}
-        }
-    }
-
-    // Method to append a null keyframe to the curve
-    pub fn append_null_keyframe(&mut self) {
-        match self {
-            Curve::KeyframeVector3(keyframes) => {
-                if let Some(last_frame) = keyframes.last().map(|keyframe| keyframe.frame) {
-                    if last_frame != -1 {
-                        let null_keyframe = KeyframeVector3 {
-                            frame: -1,
-                            value: keyframes.last().unwrap().value.clone(),
-                        };
-                        keyframes.push(null_keyframe);
-                    }
-                }
-            }
-            Curve::KeyframeVector4(keyframes) => {
-                if let Some(last_frame) = keyframes.last().map(|keyframe| keyframe.frame) {
-                    if last_frame != -1 {
-                        let null_keyframe = KeyframeVector4 {
-                            frame: -1,
-                            value: keyframes.last().unwrap().value.clone(),
-                        };
-                        keyframes.push(null_keyframe);
-                    }
-                }
-            }
-            Curve::KeyframeFloat(keyframes) => {
-                if let Some(last_frame) = keyframes.last().map(|keyframe| keyframe.frame) {
-                    if last_frame != -1 {
-                        let null_keyframe = KeyframeFloat {
-                            frame: -1,
-                            value: keyframes.last().unwrap().value.clone(),
-                        };
-                        keyframes.push(null_keyframe);
-                    }
-                }
-            }
-            _ => {} // No null keyframe for other curve variants
-        }
-    }
-}
-
-fn from_iterator_args<'it, R, T, Arg, Ret, It>(
+fn read_curve<'it, R, T, Arg, Ret, It>(
     it: It,
 ) -> impl FnOnce(&mut R, &ReadOptions, ()) -> BinResult<Ret>
 where
@@ -365,6 +252,86 @@ where
             .collect()
     }
 }
+
+fn write_curve<R: Write + Seek>(
+    curve_format: CurveFormat,
+    values: Math,
+    writer: &mut R,
+    wo: &WriteOptions,
+    
+    _: ()
+) -> BinResult<()> {
+
+    match (curve_format, values) {
+        (CurveFormat::Vector3Fixed, Math::Vec3 { channels }) |
+        (CurveFormat::EulerXYZFixed, Math::Vec3 { channels }) |
+        (CurveFormat::Vector3Table, Math::Vec3 { channels }) => {
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+        }
+    
+        (CurveFormat::Vector3Linear, Math::Vec3Linear { frame, channels }) => {
+            frame.write_options(writer, wo, ())?;
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::QuaternionLinear, Math::Vec4Linear { frame, channels }) => {
+            frame.write_options(writer, wo, ())?;
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+            channels.3.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::FloatFixed, Math::Float { channels }) |
+        (CurveFormat::FloatTable, Math::Float { channels}) |
+        (CurveFormat::FloatTableNoInterp, Math::Float { channels}) => {
+            channels.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::FloatLinear, Math::FloatLinear { frame, channels }) => {
+            frame.write_options(writer, wo, ())?;
+            channels.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::OpacityShortTable, Math::I16Vec { channels }) |
+        (CurveFormat::OpacityShortTableNoInterp, Math::I16Vec { channels }) => {
+            channels.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::ScaleShortTable, Math::I16Vec3 { channels }) => {
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::QuaternionShortTable, Math::I16Vec4 { channels }) |
+        (CurveFormat::QuaternionShortTableNoInterp, Math::I16Vec4 { channels }) => {
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+            channels.3.write_options(writer, wo, ())?;
+        }
+
+        (CurveFormat::ColorRGBTable, Math::Color { channels }) => {
+            channels.0.write_options(writer, wo, ())?;
+            channels.1.write_options(writer, wo, ())?;
+            channels.2.write_options(writer, wo, ())?;
+        }
+        // Handle other CurveFormat cases and Keyframe variants...
+        _ => todo!(),
+    }
+    
+
+
+    Ok(())
+}
+
+
+
 
 impl NuccChunk for NuccChunkAnm {
     fn chunk_type(&self) -> NuccChunkType {
